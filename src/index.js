766bbfcc74a8083fa73d5ad8dd4457109bdfa7d3
Tiago Amaro <tiagopadrela@gmail.com>
2017-03-06 14:44:12 +0100

    change term for searching MDN

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const icon = require('./icon.png')
-const mdnRegex = /mdn\s+(.*)/i
+const icon     = require('./icon.png');
+const mdnRegex = /mdn\s+(.*)/i;
 
 const plugin = ({term, display, actions}) => {
   if (mdnRegex.test(term)) {
     const openMdn = (query) => {
-      let q = encodeURIComponent(query)
-      actions.open(`https://developer.mozilla.org/search?q=${q}`)
+      let q = encodeURIComponent(query);
+      actions.open(`https://developer.mozilla.org/search?q=${q}`);
       actions.hideWindow()
-    }
+    };
 
-    let [, mdnQuery] = mdnRegex.exec(term)
+    let [, mdnQuery] = mdnRegex.exec(term);
 
     display({
-      icon: icon,
-      title: `Open MDN for ${mdnQuery}`,
+      icon:     icon,
+      title:    `Search MDN for ${mdnQuery}`,
       onSelect: () => openMdn(mdnQuery)
     })
   }
@@ -23,4 +23,4 @@ const plugin = ({term, display, actions}) => {
 
 module.exports = {
   fn: plugin
-}
+};
